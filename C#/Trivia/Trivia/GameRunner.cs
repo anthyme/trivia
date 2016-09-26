e966f98fd1711abf0f45f41de715a0f5e7fd671a
anthyme <anthyme@gmail.com>
2016-09-26 23:18:42 +0200

    Add ouput to textfile

--- C#/Trivia/Trivia/GameRunner.cs
+++ C#/Trivia/Trivia/GameRunner.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using Trivia.Core;
 
 namespace Trivia
@@ -10,38 +9,35 @@ namespace Trivia
 
         private static bool notAWinner;
 
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
-            Game aGame = new Game();
-
-            aGame.add("Chet");
-            aGame.add("Pat");
-            aGame.add("Sue");
-
-            Random rand = new Random();
-
-            do
+            using (var stream = File.OpenWrite("output.txt"))
+            using (var writer = new StreamWriter(stream))
             {
+                Console.SetOut(writer);
+                Game aGame = new Game();
 
-                aGame.roll(rand.Next(5) + 1);
+                aGame.add("Chet");
+                aGame.add("Pat");
+                aGame.add("Sue");
 
-                if (rand.Next(9) == 7)
+                Random rand = new Random();
+
+                do
                 {
-                    notAWinner = aGame.wrongAnswer();
-                }
-                else
-                {
-                    notAWinner = aGame.wasCorrectlyAnswered();
-                }
+                    aGame.roll(rand.Next(5) + 1);
 
-
-
-            } while (notAWinner);
-
+                    if (rand.Next(9) == 7)
+                    {
+                        notAWinner = aGame.wrongAnswer();
+                    }
+                    else
+                    {
+                        notAWinner = aGame.wasCorrectlyAnswered();
+                    }
+                } while (notAWinner);
+            }
         }
-
-
     }
-
 }
 
