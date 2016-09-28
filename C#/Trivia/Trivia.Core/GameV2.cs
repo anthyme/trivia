baafdf1efcaa6ed763ea6d547a1c0bf47b8920a6
anthyme <anthyme@gmail.com>
2016-09-28 22:43:05 +0200

    move files

--- C#/Trivia/Trivia.Core/GameV2.cs
+++ C#/Trivia/Trivia.Core/GameV2.cs
@@ -4,45 +4,6 @@ using System.Linq;
 
 namespace Trivia.Core
 {
-    public interface IGame
-    {
-        bool AddPlayer(string name);
-        void Roll(int value);
-        bool WrongAnswer();
-        bool CorrectAnswer();
-    }
-
-    public class LegacyGameAdapter : IGame
-    {
-        private readonly Game game;
-
-        public LegacyGameAdapter()
-        {
-            game = new Game();
-        }
-
-        public bool AddPlayer(string name)
-        {
-            return game.add(name);
-        }
-
-        public void Roll(int value)
-        {
-            game.roll(value);
-        }
-
-        public bool WrongAnswer()
-        {
-            return game.wrongAnswer();
-        }
-
-        public bool CorrectAnswer()
-        {
-            return game.wasCorrectlyAnswered();
-        }
-    }
-
-
     public class GameV2 : IGame
     {
         private readonly List<string> players = new List<string>();
