abdc09173864a1359a8b4e600295246bf738ef63
anthyme <anthyme@gmail.com>
2016-09-28 13:55:41 +0200

    Add test of legacy game with good coverage

---
+++ C#/Trivia/Trivia.Tests/TestOldGame.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Trivia.Core;
+using Xunit;
+
+namespace Trivia.Tests
+{
+    public class TestOldGame
+    {
+        [Fact]
+        public void TestGame()
+        {
+            var game = new Game();
+
+            using (var file = File.OpenWrite("subject.txt"))
+            using (var writer = new StreamWriter(file))
+            {
+                Console.SetOut(writer);
+
+                game.add("Chet");
+                game.add("Pat");
+                game.add("Sue");
+                game.roll(4);
+                game.wasCorrectlyAnswered();
+                game.roll(5);
+                game.wasCorrectlyAnswered();
+                game.roll(2);
+                game.wasCorrectlyAnswered();
+                game.roll(4);
+                game.wrongAnswer();
+                game.roll(4);
+                game.wasCorrectlyAnswered();
+                game.roll(5);
+                game.wasCorrectlyAnswered();
+                game.roll(2);
+                game.wasCorrectlyAnswered();
+                game.roll(2);
+                game.wasCorrectlyAnswered();
+                game.roll(2);
+                game.wasCorrectlyAnswered();
+                game.roll(1);
+                game.wasCorrectlyAnswered();
+                game.roll(1);
+                game.wasCorrectlyAnswered();
+                game.roll(3);
+                game.wrongAnswer();
+                game.roll(3);
+                game.wrongAnswer();
+                game.roll(1);
+                game.wasCorrectlyAnswered();
+                game.roll(5);
+                game.wasCorrectlyAnswered();
+                game.roll(3);
+                game.wasCorrectlyAnswered();
+                game.roll(5);
+                game.wasCorrectlyAnswered();
+            }
+
+            var subject = File.ReadAllLines("subject.txt").ToArray();
+
+            File.WriteAllText("expected.txt", ExpectedContents);
+            var expectedLines = File.ReadAllLines("expected.txt").ToArray();
+
+            subject.ShouldAllBeEquivalentTo(expectedLines);
+        }
+
+        private const string ExpectedContents = 
+@"Chet was added
+They are player number 1
+Pat was added
+They are player number 2
+Sue was added
+They are player number 3
+Chet is the current player
+They have rolled a 4
+Chet's new location is 4
+The category is Pop
+Pop Question 0
+Answer was corrent!!!!
+Chet now has 1 Gold Coins.
+Pat is the current player
+They have rolled a 5
+Pat's new location is 5
+The category is Science
+Science Question 0
+Answer was corrent!!!!
+Pat now has 1 Gold Coins.
+Sue is the current player
+They have rolled a 2
+Sue's new location is 2
+The category is Sports
+Sports Question 0
+Answer was corrent!!!!
+Sue now has 1 Gold Coins.
+Chet is the current player
+They have rolled a 4
+Chet's new location is 8
+The category is Pop
+Pop Question 1
+Question was incorrectly answered
+Chet was sent to the penalty box
+Pat is the current player
+They have rolled a 4
+Pat's new location is 9
+The category is Science
+Science Question 1
+Answer was corrent!!!!
+Pat now has 2 Gold Coins.
+Sue is the current player
+They have rolled a 5
+Sue's new location is 7
+The category is Rock
+Rock Question 0
+Answer was corrent!!!!
+Sue now has 2 Gold Coins.
+Chet is the current player
+They have rolled a 2
+Chet is not getting out of the penalty box
+Pat is the current player
+They have rolled a 2
+Pat's new location is 11
+The category is Rock
+Rock Question 1
+Answer was corrent!!!!
+Pat now has 3 Gold Coins.
+Sue is the current player
+They have rolled a 2
+Sue's new location is 9
+The category is Science
+Science Question 2
+Answer was corrent!!!!
+Sue now has 3 Gold Coins.
+Chet is the current player
+They have rolled a 1
+Chet is getting out of the penalty box
+Chet's new location is 9
+The category is Science
+Science Question 3
+Answer was correct!!!!
+Chet now has 2 Gold Coins.
+Pat is the current player
+They have rolled a 1
+Pat's new location is 0
+The category is Pop
+Pop Question 2
+Answer was corrent!!!!
+Pat now has 4 Gold Coins.
+Sue is the current player
+They have rolled a 3
+Sue's new location is 0
+The category is Pop
+Pop Question 3
+Question was incorrectly answered
+Sue was sent to the penalty box
+Chet is the current player
+They have rolled a 3
+Chet is getting out of the penalty box
+Chet's new location is 0
+The category is Pop
+Pop Question 4
+Question was incorrectly answered
+Chet was sent to the penalty box
+Pat is the current player
+They have rolled a 1
+Pat's new location is 1
+The category is Science
+Science Question 4
+Answer was corrent!!!!
+Pat now has 5 Gold Coins.
+Sue is the current player
+They have rolled a 5
+Sue is getting out of the penalty box
+Sue's new location is 5
+The category is Science
+Science Question 5
+Answer was correct!!!!
+Sue now has 4 Gold Coins.
+Chet is the current player
+They have rolled a 3
+Chet is getting out of the penalty box
+Chet's new location is 3
+The category is Rock
+Rock Question 2
+Answer was correct!!!!
+Chet now has 3 Gold Coins.
+Pat is the current player
+They have rolled a 5
+Pat's new location is 6
+The category is Sports
+Sports Question 1
+Answer was corrent!!!!
+Pat now has 6 Gold Coins.";
+    }
+}
--- C#/Trivia/Trivia/GameRunner.cs
+++ C#/Trivia/Trivia/GameRunner.cs
@@ -11,10 +11,12 @@ namespace Trivia
 
         public static void Main(string[] args)
         {
-            using (var stream = File.OpenWrite("output.txt"))
-            using (var writer = new StreamWriter(stream))
+            using (var stream1 = File.OpenWrite("expected.txt"))
+            using (var writer1 = new StreamWriter(stream1))
+            using (var stream2 = File.OpenWrite("code.txt"))
+            using (var writer2 = new StreamWriter(stream2))
             {
-                Console.SetOut(writer);
+                Console.SetOut(writer1);
                 Game aGame = new Game();
 
                 aGame.add("Chet");
@@ -25,15 +27,19 @@ namespace Trivia
 
                 do
                 {
-                    aGame.roll(rand.Next(5) + 1);
+                    var roll = rand.Next(5) + 1;
+                    aGame.roll(roll);
+                    writer2.WriteLine($"game.roll({roll});");
 
                     if (rand.Next(9) == 7)
                     {
                         notAWinner = aGame.wrongAnswer();
+                        writer2.WriteLine("game.wrongAnswer();");
                     }
                     else
                     {
                         notAWinner = aGame.wasCorrectlyAnswered();
+                        writer2.WriteLine("game.wasCorrectlyAnswered();");
                     }
                 } while (notAWinner);
             }
